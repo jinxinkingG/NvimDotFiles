91875e0a6f114caca9ccfa19fcd6313cd99d2c27
jinxin <jinxin@it.lianfa.cn>
2023-07-19 14:59:04 +0800

    colorscheme change

--- lua/configs/mason-lspconfig.lua
+++ lua/configs/mason-lspconfig.lua
@@ -1,12 +1,15 @@
 require("neodev").setup({})
 local lspconfig = require('lspconfig')
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 require("mason-lspconfig").setup()
 require("mason-lspconfig").setup_handlers {
         -- The first entry (without a key) will be the default handler
         -- and will be called for each installed server that doesn't have
         -- a dedicated handler.
         function (server_name) -- default handler (optional)
-            lspconfig[server_name].setup {}
+            lspconfig[server_name].setup {
+		    capabilities = capabilities
+	    }
         end,
 	-- java-language-server
 	["java_language_server"] = function()
@@ -22,6 +25,7 @@ require("mason-lspconfig").setup_handlers {
         ["lua_ls"] = function ()
 		--lua-language-server
 		lspconfig.lua_ls.setup{
+			capabilities = capabilities,
 			settings = {
 				Lua = {
 					completion = {
@@ -49,6 +53,7 @@ require("mason-lspconfig").setup_handlers {
 	end,
 	["jsonls"] = function()
 		lspconfig.jsonls.setup{
+			capabilities = capabilities,
 			settings = {
 				json = {
 					schemas = require('schemastore').json.schemas(),
